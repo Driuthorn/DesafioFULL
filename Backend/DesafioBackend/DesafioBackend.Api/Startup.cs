6d7c7a55f8ae13eaa11ac03d782610936b0f25ec
Driuthorn <Douglas_doAmaral@hotmail.com>
2021-04-29 00:25:27 -0300

    Adiciona Injeção de dependencia.
    * Implementa a injeção de dependencia com o SimpleInjector.

--- Backend/DesafioBackend/DesafioBackend.Api/Startup.cs
+++ Backend/DesafioBackend/DesafioBackend.Api/Startup.cs
@@ -1,14 +1,21 @@
+using DesafioBackend.Api.Infrastructure.IoC;
+using DesafioBackend.Data.Database.Context;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
+using SimpleInjector;
+using SimpleInjector.Lifestyles;
 
 namespace DesafioBackend.Api
 {
     public class Startup
     {
+        public Container _dependencyInjectionContainer { get; } = new Container();
+        public Bootstrapper _bootstrapper { get; } = new Bootstrapper();
+
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
@@ -23,6 +30,20 @@ namespace DesafioBackend.Api
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "DesafioBackend", Version = "v1" });
             });
+
+            _dependencyInjectionContainer.Options.AllowOverridingRegistrations = true;
+            _dependencyInjectionContainer.Options.DefaultLifestyle = new AsyncScopedLifestyle();
+
+            services.AddSimpleInjector(_dependencyInjectionContainer, options =>
+            {
+                options.AddAspNetCore()
+                    .AddControllerActivation();
+
+                options.AddLogging();
+            });
+
+            services.AddScoped<DesafioBackendContext>();
+            services.AddSingleton(Configuration);
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -33,7 +54,8 @@ namespace DesafioBackend.Api
             }
 
             app.UseRouting();
-
+            _bootstrapper.Inject(_dependencyInjectionContainer);
+            app.UseSimpleInjector(_dependencyInjectionContainer);
             app.UseSwagger();
             app.UseSwaggerUI(c =>
             {
@@ -45,6 +67,8 @@ namespace DesafioBackend.Api
                 endpoints.MapControllers();
                 endpoints.MapControllerRoute("default", "{controller}/{action}");
             });
+
+            _dependencyInjectionContainer.Verify();
         }
     }
 }
