b96d95e119d37038e715dd58e362c97a14d02ab2
Driuthorn <Douglas_doAmaral@hotmail.com>
2021-04-29 00:39:49 -0300

    Adiciona classe genérica de exceção.

---
+++ Backend/DesafioBackend/DesafioBackend.CrossCutting.Exceptions/ApiCustomException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Net;
+
+namespace DesafioBackend.CrossCutting.Exceptions
+{
+    public class ApiCustomException : Exception
+    {
+        public HttpStatusCode ResponseCode { get; }
+        public string CustomMessage { get; }
+
+        public ApiCustomException(string customMessage, HttpStatusCode responseCode)
+        {
+            CustomMessage = customMessage;
+            ResponseCode = responseCode;
+        }
+    }
+}
