d3c6f6dec3e75db32944cc35f3cf7e44df92f278
Driuthorn <Douglas_doAmaral@hotmail.com>
2021-04-29 00:33:32 -0300

    Implementa método de recuperação de titulo.

---
+++ Backend/DesafioBackend/DesafioBackend.Data.Query/Queries/v1/Titulo/Get/GetTituloQueryResponse.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DesafioBackend.Data.Query.Queries.v1.Titulo.Get
+{
+    public class GetTituloQueryResponse
+    {
+        public GetTituloQueryResponse()
+        {
+            Titulos = new List<Titulo>();
+        }
+        public List<Titulo> Titulos { get; set; }
+
+        #region Nested Classes
+        public class Titulo
+        {
+            public Guid Id { get; set; } 
+            public int NumeroTitulo { get; set; }
+            public string NomeDevedor { get; set; }
+            public int QtdParcelas { get; set; }
+            public decimal ValorOriginal { get; set; }
+            public int DiasEmAtraso { get; set; }
+            public decimal ValorAtual { get; set; }
+            public List<Parcela> Parcelas { get; set; }
+        }
+
+        public class Parcela
+        {
+            public int NumeroParcela { get; set; }
+            public decimal Valor { get; set; }
+            public DateTime Vencimento { get; set; }
+        }
+        #endregion
+    }
+}
