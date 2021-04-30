ee44f8a7648add33bfd72fa269b47f0a38568423
Driuthorn <Douglas_doAmaral@hotmail.com>
2021-04-30 04:23:28 -0300

    Atualização de imports e chamada de componente no app.
    * Adicionado a implementação do componente do Toast ao app.component.
    * Adicionado ao appmodule declaração do componente Toast, a importação do SharedModule, ngbModule eNgxMaskModulo, e adicionado aos providers o ModalService.

--- Frontend/DesafioFrontend/src/app/app.module.ts
+++ Frontend/DesafioFrontend/src/app/app.module.ts
@@ -8,31 +8,40 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/shared/header/header.component';
 import { LoaderComponent } from './components/shared/loader/loader.component';
 import { TituloManagementRoutingModule } from './pages/titulo-management/module/titulo-management-routing.module';
+
+import { SharedModule } from './shared.module';
+
 // Services
 import { HttpService } from '../app/core/http.service';
+import { ModalService } from '../app/service/modal.service';
 import { LoaderService } from '../app/service/loader.service';
 import { LoaderInterceptor } from '../app/service/interceptors/loader-interceptor.service';
-import { FilterComponent } from './components/elements/filter/filter.component';
-
+import { ToastMessageComponent } from './components/elements/toast-message/toast-message.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgxMaskModule } from 'ngx-mask';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     LoaderComponent,
-    FilterComponent,
+    ToastMessageComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    NgbModule,
     HttpClientModule,
     BrowserAnimationsModule,
-    TituloManagementRoutingModule
+    TituloManagementRoutingModule,
+    SharedModule,
+    NgxMaskModule.forRoot(),
   ],
   providers: [
     HttpService,
     LoaderService,
     { provide: HTTP_INTERCEPTORS, useClass: LoaderInterceptor, multi: true },
+    ModalService
   ],
   bootstrap: [AppComponent]
 })
